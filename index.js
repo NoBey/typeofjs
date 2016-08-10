aab485000195f68ad830fe0ab85d85b423e58f8a
NoBey <786964300@qq.com>
2016-08-11 02:07:50 +0800

    ok

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-export default function () {
-  console.log('default');
+export default function(object) {
+  var toString = Object.prototype.toString;
+  var type = typeof object;
+
+  if (type === 'undefined') {
+    return 'undefined';
+  }
+
+  if (object) {
+    type = object.constructor.name;
+  } else if (type === 'object') {
+    type = toString.call(object).slice(8, -1);
+  }
+
+  return type.toLowerCase();
 }
+// module.exports
